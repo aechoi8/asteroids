ca75bd1f46b4081ecca810964842e83fb380e746
albertchoi8 <aebchoi@gmail.com>
2025-03-03 08:46:00 -0800

    add constants/infinite loop

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #inifinite while loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
